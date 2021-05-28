2c5de264879a87d2effea059f13cc4b5d2f3d0ed
Saulo Coelho <coelho.saulo@gmail.com>
2021-05-28 15:26:58 -0300

    check answer

--- src/App.js
+++ src/App.js
@@ -1,36 +1,98 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
 import {RadioButton, Button} from 'react-native-paper';
 
 const App = () => {
-  const [value, setValue] = React.useState('');
+  const [value, setValue] = useState('');
+  //const [pontos, setPontos] = useState(0);
+  const [i, setI] = useState(0);
+
   const data = [
-    'Alternativa a',
-    'Alternativa b',
-    'Alternativa c',
-    'Alternativa d',
-    'Alternativa e',
+    {
+      pergunta:
+        'Os alimentos ricos em fibras regularizam o funcionamento do intestino. Indique o item cujo alimento contém mais fibra.',
+      resposta: [
+        'refrigerante',
+        'Pastel de carne',
+        'batata frita',
+        'dogão',
+        'legumes assados',
+      ],
+      certa: 'e',
+    },
+    {
+      pergunta: 'O que é Ortorexia.',
+      resposta: [
+        'Obsessão pelo consumo de alimentos saudáveis',
+        'Transtorno alimentar caracterizado pela perda de apetite',
+        'Obsessão em falar de forma correta',
+        'Preocupação exagerada em ter um corpo elegante',
+        'Distúrbiovalimentar que provoca a vingestão excessiva de alimentos',
+      ],
+      certa: 'a',
+    },
   ];
 
+  //console.log(data);
+
+  const onSave = () => {
+    i === 0 ? setI(1) : setI(0);
+    if (value === data[i].certa) {
+      console.log('correta');
+    } else {
+      console.log('incorreta');
+    }
+  };
+
   return (
-    <View>
-      <Text>
-        Os alimentos ricos em fibras regularizam o funcionamento do intestino.
-        Indique o item cujo alimento contém mais fibra.
-      </Text>
+    <View style={styles.container}>
+      <Text style={styles.textAsk}>{data[i].pergunta}</Text>
       <RadioButton.Group onValueChange={value => setValue(value)} value={value}>
-        <RadioButton.Item label={data[0]} value="a" />
-        <RadioButton.Item label={data[1]} value="b" />
-        <RadioButton.Item label={data[2]} value="c" />
-        <RadioButton.Item label={data[3]} value="d" />
+        <RadioButton.Item
+          labelStyle={styles.textAnswer}
+          label={data[i].resposta[0]}
+          value="a"
+        />
+        <RadioButton.Item
+          labelStyle={styles.textAnswer}
+          label={data[i].resposta[1]}
+          value="b"
+        />
+        <RadioButton.Item
+          labelStyle={styles.textAnswer}
+          label={data[i].resposta[2]}
+          value="c"
+        />
+        <RadioButton.Item
+          labelStyle={styles.textAnswer}
+          label={data[i].resposta[3]}
+          value="d"
+        />
+        <RadioButton.Item
+          labelStyle={styles.textAnswer}
+          label={data[i].resposta[4]}
+          value="e"
+        />
       </RadioButton.Group>
-      <Button
-        mode="contained"
-        onPress={() => console.log('avançar :: ' + value)}>
+      <Button mode="contained" onPress={onSave}>
         Avançar
       </Button>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+  },
+  textAsk: {
+    fontSize: 24,
+    textAlign: 'justify',
+  },
+  textAnswer: {
+    fontSize: 24,
+  },
+});
 
 export default App;
