e6235a02d3520c776186a14ec7d846da1bc3168e
Saulo Coelho <coelho.saulo@gmail.com>
2021-06-02 17:11:51 -0300

    update ControlScreen

--- src/pages/Control.js
+++ src/pages/Control.js
@@ -1,12 +1,68 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, StyleSheet} from 'react-native';
 
-const Control = () => {
+import {TextInput, Button} from 'react-native-paper';
+
+//import firestore from '@react-native-firebase/firestore';
+
+const Control = ({navigation}) => {
+  const [password, setPassword] = useState('');
+  const [nameRoom, setNameRoom] = useState('');
+
+  const startGame = () => {
+    console.log(`startGame :: senha: ${password}, sala: ${nameRoom}`);
+  };
+
   return (
-    <View>
-      <Text>Control Screen</Text>
+    <View style={styles.container}>
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          label="Senha do Administrador"
+          value={password}
+          onChangeText={text => setPassword(text)}
+        />
+
+        <TextInput
+          style={styles.input}
+          label="Nome da Sala"
+          value={nameRoom}
+          onChangeText={text => setNameRoom(text)}
+        />
+
+        <Button mode="contained" onPress={startGame} color="#2980b9">
+          Criar Sala
+        </Button>
+
+        <Button
+          style={styles.button}
+          mode="contained"
+          onPress={() => {
+            navigation.goBack();
+          }}
+          color="#2980b9">
+          Cancelar
+        </Button>
+      </View>
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    //margin: 10,
+    justifyContent: 'center',
+    backgroundColor: '#3498db',
+  },
+  form: {
+    //flex: 1,
+  },
+  input: {
+    marginBottom: 5,
+  },
+  button: {
+    marginTop: 10,
+  },
+});
 export default Control;
