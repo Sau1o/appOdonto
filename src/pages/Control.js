deaeba5780455cc1d32f31644230f6165da0954f
Saulo Coelho <coelho.saulo@gmail.com>
2021-06-04 19:36:06 -0300

    add Fisher Yates Method

--- src/pages/Control.js
+++ src/pages/Control.js
@@ -14,13 +14,27 @@ const Control = ({navigation}) => {
   const startGame = async () => {
     //console.log(`startGame :: senha: ${password}, sala: ${nameRoom}`);
     if (password === pass) {
-      firestore()
+      /*firestore()
         .collection('sala')
         .doc(nameRoom)
         .set({})
         .then(() => {
           console.log('User added!');
-        });
+        });*/
+      //const users = await firestore().collection('quiz').get();
+      //const user = {...users.docs[1].data()};
+      //console.log(user);
+
+      const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+      var i, j, k;
+
+      for (i = array.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * i);
+        k = array[i];
+        array[i] = array[j];
+        array[j] = k;
+      }
+      console.log(array.slice(0, 10));
     } else {
       // eslint-disable-next-line no-alert
       alert('Senha incorreta, tente novamente');
