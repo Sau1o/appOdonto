bdc7bfa759caba4dabb3882a45ba56f44ba7f67f
Saulo Coelho <coelho.saulo@gmail.com>
2021-06-15 18:03:48 -0300

    recover filtred data

--- src/pages/Scores.js
+++ src/pages/Scores.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, FlatList, Text} from 'react-native';
 
 import {Button} from 'react-native-paper';
 
 import firestore from '@react-native-firebase/firestore';
 
 const Scores = () => {
+  const [scores, setScores] = React.useState([]);
   const newGame = async () => {
     let querySnapshot;
     querySnapshot = await firestore()
@@ -13,16 +14,30 @@ const Scores = () => {
       .orderBy('pontos', 'desc')
       .get();
 
-    let entries = querySnapshot.docs.map(documentSnapshot =>
+    var entries = querySnapshot.docs.map(documentSnapshot =>
       documentSnapshot.data(),
     );
-    console.log(entries);
+    console.log('newGame :: ', entries);
+    //return entries;
+    setScores(entries);
   };
+
+  console.log('Scores', scores);
+
   return (
     <View>
       <Button mode="contained" onPress={newGame} color="#2980b9">
         Iniciar o Jogo
       </Button>
+      <FlatList
+        data={scores}
+        keyExtractor={item => item.name}
+        renderItem={({item}) => (
+          <View>
+            <Text>{item.name}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 };
