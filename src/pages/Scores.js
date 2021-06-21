3907ed5a7a0aa13641e255f2839f81816a4d77a7
Saulo Coelho <coelho.saulo@gmail.com>
2021-06-21 20:20:41 -0300

    update Score.js

--- src/pages/Scores.js
+++ src/pages/Scores.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, FlatList, Text} from 'react-native';
+import {View, FlatList, Text, StyleSheet} from 'react-native';
 
 import {Button} from 'react-native-paper';
 
@@ -32,19 +32,51 @@ const Scores = () => {
       <Button mode="contained" onPress={newGame} color="#2980b9">
         Iniciar o Jogo
       </Button>
+
+      <View style={styles.container}>
+        <Text style={styles.itemPos}>Pos</Text>
+
+        <Text style={styles.itemName}>Nome</Text>
+
+        <Text style={styles.itemMail}>Email</Text>
+
+        <Text style={styles.itemPontos}>Pontos</Text>
+      </View>
+
       <FlatList
         data={scores}
         keyExtractor={item => item.name}
         renderItem={({item}) => (
-          <View>
-            <Text>
+          <View style={styles.containerItem}>
+            <Text style={styles.itemPos}>
               {scores.findIndex(scores => scores.name === item.name) + 1}
             </Text>
+
+            <Text style={styles.itemName}>{item.name}</Text>
+
+            <Text style={styles.itemMail}>{item.email}</Text>
+
+            <Text style={styles.itemPontos}>{item.pontos}</Text>
           </View>
         )}
       />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    //flex: 1,
+    flexDirection: 'row',
+  },
+  containerItem: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  itemPos: {flex: 1},
+  itemName: {flex: 1},
+  itemMail: {flex: 1},
+  itemPontos: {flex: 1},
+});
 
 export default Scores;
